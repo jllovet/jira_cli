571aff5e9d925915acb12fe1ab882f37f8f283a7
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-10-01 00:29:09 -0400

    Add user group command stub

--- commands/user/__init__.py
+++ commands/user/__init__.py
@@ -1,8 +1,10 @@
 import click
 from .avatar import avatar_cmd
+from .group import group_cmd
 
 @click.group(name="user", help="Manage users")
 def user_cmd():
     raise click.UsageError("This command hasn't been implemented yet")
 
 user_cmd.add_command(avatar_cmd)
+user_cmd.add_command(group_cmd)
