92ee777eaf2d1d04a201b57b8a29abad4ec547e6
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-10-01 00:19:09 -0400

    Add issue and project permission command stubs

--- commands/issue/__init__.py
+++ commands/issue/__init__.py
@@ -6,6 +6,7 @@ from .field import field_cmd
 from .watcher import watcher_cmd
 from .assignee import assignee_cmd
 from .reporter import reporter_cmd
+from .permission import permission_cmd
 
 
 @click.group(name="issue", help="Manage issues")
@@ -20,3 +21,4 @@ issue_cmd.add_command(field_cmd)
 issue_cmd.add_command(watcher_cmd)
 issue_cmd.add_command(assignee_cmd)
 issue_cmd.add_command(reporter_cmd)
+issue_cmd.add_command(permission_cmd)
