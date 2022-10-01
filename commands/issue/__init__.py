41633727605713f52a34783b8a0c5d94393bbc94
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-09-30 23:51:53 -0400

    Add issue watcher command stub

--- commands/issue/__init__.py
+++ commands/issue/__init__.py
@@ -3,6 +3,7 @@ from .attachment import attachment_cmd
 from .worklog import worklog_cmd
 from .label import label_cmd
 from .field import field_cmd
+from .watcher import watcher_cmd
 
 
 @click.group(name="issue", help="Manage issues")
@@ -14,3 +15,4 @@ issue_cmd.add_command(attachment_cmd)
 issue_cmd.add_command(worklog_cmd)
 issue_cmd.add_command(label_cmd)
 issue_cmd.add_command(field_cmd)
+issue_cmd.add_command(watcher_cmd)
