40af31ce875a2f92db6d13034fef52e18d431ecb
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-09-30 23:56:58 -0400

    Add issue assignee and reporter command stubs

--- commands/issue/__init__.py
+++ commands/issue/__init__.py
@@ -4,6 +4,8 @@ from .worklog import worklog_cmd
 from .label import label_cmd
 from .field import field_cmd
 from .watcher import watcher_cmd
+from .assignee import assignee_cmd
+from .reporter import reporter_cmd
 
 
 @click.group(name="issue", help="Manage issues")
@@ -16,3 +18,5 @@ issue_cmd.add_command(worklog_cmd)
 issue_cmd.add_command(label_cmd)
 issue_cmd.add_command(field_cmd)
 issue_cmd.add_command(watcher_cmd)
+issue_cmd.add_command(assignee_cmd)
+issue_cmd.add_command(reporter_cmd)
