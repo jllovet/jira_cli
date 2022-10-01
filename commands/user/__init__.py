323e48bfbabcf79165ce5c8c7f9650095d53ee4b
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-10-01 00:24:07 -0400

    Add user avatar command stub

--- commands/user/__init__.py
+++ commands/user/__init__.py
@@ -1,6 +1,8 @@
 import click
-
+from .avatar import avatar_cmd
 
 @click.group(name="user", help="Manage users")
 def user_cmd():
     raise click.UsageError("This command hasn't been implemented yet")
+
+user_cmd.add_command(avatar_cmd)
