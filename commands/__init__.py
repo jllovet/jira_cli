98c2e8103a66a566456776c69535b4f623ce5eea
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-09-30 23:08:24 -0400

    Add group command stub

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,6 @@
 import click
 from .user import user_cmd
+from .group import group_cmd
 from .board import board_cmd
 from .issue import issue_cmd
 from .project import project_cmd
@@ -7,6 +8,7 @@ from .sprint import sprint_cmd
 
 command_groups = [
     user_cmd,
+    group_cmd,
     board_cmd,
     issue_cmd,
     project_cmd,
